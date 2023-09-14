3637c4f5eda570df498ba160b0791e8b3ac899f7
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-14 17:18:39 +1200

    Add CSS to Home page

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom"
-
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <>
-      <h1 className="catagory-choose">Blicky</h1>
-      <button className="home-button">
-        <Link to="/catagory"> Choose Game Mode </Link>
-      </button>
-    </>
+    <div>
+      <div className="flex justify-center items-center gap-80">
+        <img className="w-64 h-full" src="images/gun.png" alt="gun" />
+        <h1 className="text-8xl m-4 text-primary">Blicky</h1>
+        <img
+          className="w-64 h-full transform -scale-x-100"
+          src="images/gun.png"
+          alt="gun"
+        />
+      </div>
+      <hr className="border-4 border-primary" />
+      <div className="flex justify-center items-center h-screen bg-pink4">
+        <Link
+          to="/catagory"
+          className="border-4 rounded text-4xl border-primary px-36 py-24 text-primary hover:bg-primary hover:text-pink4 hover:animate-pulse"
+        >
+          Choose Game Mode
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./*.html', './client/**/*.[tj]sx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF5D8F',
+        pink1: '#FF87AB',
+        pink2: '#FFA6C1',
+        pink3: '#FFC4D6',
+        pink4: '#FADDE1',
+      },
+      scale: {
+        '-100': '-1',
+      },
+    },
   },
   plugins: [],
 }
