e9588fd490131b5cbc7d092be31d089773768be9
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-15 11:17:28 +1200

    Add CSS to category page

--- client/components/Catagory.tsx
+++ client/components/Catagory.tsx
@@ -1,25 +1,36 @@
-import { Link } from "react-router-dom"
-
+import { Link } from 'react-router-dom'
+import Header from './Header'
 
 function Catagory() {
   return (
     <>
-      <h1 className="catagory-choose">Game Modes</h1>
-      <button className="home-button">
-        <Link to="/blicky"> Blicky </Link>
-      </button>
-      <button className="home-button">
-        <Link to="/2"> Game 2/ Difficulty </Link>
-      </button>
-      <button className="home-button">
-        <Link to="/3"> Game 3/ Difficulty </Link>
-      </button>
-      <button className="home-button">
-        <Link to="/4"> Game 4/ Difficulty </Link>
-      </button>
-      <button className="home-button">
-        <Link to="/5"> Game 5/ Difficulty </Link>
-      </button>
+      <Header />
+      <h2 className="text-5xl m-12 text-primary font-bold text-center">
+        Game Modes
+      </h2>
+      <div className="flex justify-center items-center gap-96 m-6">
+        <Link className="cat-btn" to="/blicky">
+          Blicky
+        </Link>
+        <Link className="cat-btn" to="/2">
+          Game 2/ Difficulty
+        </Link>
+      </div>
+
+      <div className="flex justify-center m-6">
+        <Link className="cat-btn" to="/3">
+          Game 3/ Difficulty
+        </Link>
+      </div>
+
+      <div className="flex justify-center items-center gap-96 m-6">
+        <Link className="cat-btn" to="/4">
+          Game 4/ Difficulty
+        </Link>
+        <Link className="cat-btn" to="/5">
+          Game 5/ Difficulty
+        </Link>
+      </div>
     </>
   )
 }
