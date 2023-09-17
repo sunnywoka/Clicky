b7f8b7a407a2943a8125be2883a0f09d27a3188c
Humaid Memee <humaidmemee@gmail.com>
2023-09-18 10:31:21 +1200

    commit before moving to playwright

--- client/scoring test/scoring.test.tsx
+++ client/scoring test/scoring.test.tsx
@@ -1,18 +1,25 @@
 // @vitest-environment jsdom
-import { cleanup, screen } from '@testing-library/react'
-import path from 'path'
+import { cleanup, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
-import { afterEach, expect, describe, it } from 'vitest'
+import { afterEach, expect, describe, it, vi, beforeEach } from 'vitest'
 import Game from '../components/Game'
 import { renderComponent } from '../test-utils'
+import ExplodeMock from './ExplodeMock'
 
 afterEach(cleanup)
 
 describe('Game Scoring', () => {
-  // Mock the Explode component
-  // // beforeEach(() => {
-  // //   vi.mock('../components/Explode.jsx', () => ExplodeMock)
-  // // })
+  beforeEach(() => {
+    // vi.mock('../components/Explode.jsx', ExplodeMock)
+  })
+
+  // vi.mock('../components/Explode.jsx', () => {
+  //   return {
+  //     success: vi.fn(),
+  //     failure: vi.fn(),
+  //   }
+  // })
+
   it('should display the correct score for no clicks', async () => {
     const { user } = renderComponent(
       <MemoryRouter initialEntries={['/clicky']}>
@@ -30,23 +37,23 @@ describe('Game Scoring', () => {
     })
     expect(score.textContent).toBe(expectedScore)
   })
-  // it('should display the correct score for an immediateclick', async () => {
-  //   const { user } = renderComponent(
-  //     <MemoryRouter initialEntries={['/clicky']}>
-  //       <Routes>
-  //         <Route path="/clicky" element={<Game />} />
-  //       </Routes>
-  //     </MemoryRouter>
-  //   )
-  //   const expectedScore = 'Score: 100'
-  //   await user.click(screen.getByRole('button', { name: /Start/i }))
-  //   user.click(screen.getByTestId('square-rect'))
-  //   await waitFor(() => {
-  //     const updatedScore = screen.getByRole('heading', {
-  //       level: 2,
-  //       name: /Score:/i,
-  //     })
-  //     expect(updatedScore.textContent).toBe(expectedScore)
-  //   })
-  // })
+  it('should display the correct score for an immediateclick', async () => {
+    const { user } = renderComponent(
+      <MemoryRouter initialEntries={['/clicky']}>
+        <Routes>
+          <Route path="/clicky" element={<Game />} />
+        </Routes>
+      </MemoryRouter>
+    )
+    const expectedScore = 'Score: 100'
+    await user.click(screen.getByRole('button', { name: /Start/i }))
+    user.click(screen.getByTestId('square-rect'))
+    await waitFor(() => {
+      const updatedScore = screen.getByRole('heading', {
+        level: 2,
+        name: /Score:/i,
+      })
+      expect(updatedScore.textContent).toBe(expectedScore)
+    })
+  })
 })
