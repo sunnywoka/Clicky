6cc0d77f03278c1512b91194a03950593de59134
Yuekai Wu <yuekaixx@outlook.com>
2023-09-18 12:22:19 +1200

    finish backend route

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -12,8 +12,22 @@ import { scoreSchema } from '../../types/Score.ts'
 
 const router = express.Router()
 
+// Route to get player scores by Auth0 ID
+router.get('/players', validateAccessToken, async (req, res) => {
+  const auth0Id = req.auth?.payload.sub
+  if (!auth0Id) {
+    res.status(401).json({ message: 'Please provide an id' })
+    return
+  }
+  try {
+    const scores = await getPlayersScoresByAuth0ID(auth0Id)
+    res.json(scores)
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to retrieve player' })
+  }
+})
 // Route to get player scores by game ID
-router.get('/scores/game/:gameId', async (req, res) => {
+router.get('/:gameId', async (req, res) => {
   const { gameId } = req.params
   try {
     const scores = await getPlayersScoresByGameID(parseInt(gameId))
@@ -23,25 +37,8 @@ router.get('/scores/game/:gameId', async (req, res) => {
   }
 })
 
-// Route to get player scores by Auth0 ID
-router.get('/scores/players', validateAccessToken, async (req, res) => {
-  const auth0Id = req.auth?.payload.sub
-
-  if (!auth0Id) {
-    res.status(401).json({ message: 'Please provide an id' })
-    return
-  }
-
-  try {
-    const scores = await getPlayersScoresByAuth0ID(auth0Id)
-    res.json(scores)
-  } catch (error) {
-    res.status(500).json({ error: 'Unable to retrieve player' })
-  }
-})
-
 // Route to add a new score
-router.post('/scores', validateAccessToken, async (req, res) => {
+router.post('/', validateAccessToken, async (req, res) => {
   const auth0Id = req.auth?.payload.sub
   if (!auth0Id) {
     res.status(401).json({ message: 'Please provide an id' })
@@ -64,7 +61,7 @@ router.post('/scores', validateAccessToken, async (req, res) => {
 })
 
 // Route to get all scores
-router.get('/scores', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const scores = await getAllScores()
     res.json(scores)
