5837cd0ad70482e2e90834dbf7b86194262c1b82
Tyrone Leef <tyroneleef23@gmail.com>
2023-09-18 13:50:20 +1200

    Changed leaderboard border style

--- client/components/ScoreRanking.tsx
+++ client/components/ScoreRanking.tsx
@@ -27,33 +27,38 @@ function ScoreRanking() {
       </h2>
       <table className="mx-auto">
         <tr>
-          <th className="border-2 rounded text-4xl font-bold text-primary border-primary px-24 py-12">
+          <th className="border-l-4 border-y-4 text-4xl font-bold text-primary border-primary px-12 py-6">
             Order
           </th>
-          <th className="border-2 rounded text-4xl font-bold text-primary border-primary px-24 py-12">
+          <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
             Nickname
           </th>
-          <th className="border-2 rounded text-4xl font-bold text-primary border-primary px-24 py-12">
+          <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
             Score
           </th>
-          <th className="border-2 rounded text-4xl font-bold text-primary border-primary px-24 py-12">
+          <th className="border-y-4 border-r-4 text-4xl font-bold text-primary border-primary px-24 py-12">
             Game mode
           </th>
         </tr>
-        {records.data?.sort(compareFn)?.map((Score, index) => (
-          <tr key={Score.nickname}>
-            <th className="border-2 rounded text-4xl text-primary border-primary px-24 py-4">
+        {records.data?.sort(compareFn)?.map((Score, index, array) => (
+          <tr
+            key={Score.nickname}
+            className={
+              index === array.length - 1 ? 'border-b-2 border-primary' : ''
+            }
+          >
+            <td className="border-y-2 border-l-2 text-4xl text-primary border-primary px-24 py-4">
               {index + 1}
-            </th>
-            <th className="border-2 rounded text-4xl text-primary border-primary px-24 py-4">
+            </td>
+            <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
               {Score.nickname}
-            </th>
-            <th className="border-2 rounded text-4xl text-primary border-primary px-24 py-4">
+            </td>
+            <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
               {Score.score}
-            </th>
-            <th className="border-2 rounded text-4xl text-primary border-primary px-24 py-4">
+            </td>
+            <td className="border-y-2 border-r-2 text-4xl text-primary border-primary px-24 py-4">
               {Score.mode}
-            </th>
+            </td>
           </tr>
         ))}
       </table>
