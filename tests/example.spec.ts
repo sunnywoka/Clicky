cbdac7b793134bea8aa6b2daa6e1d27a741081da
Humaid Memee <humaidmemee@gmail.com>
2023-09-18 13:35:52 +1200

    add game modes test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('home has title', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
-  // Expect a title "to contain" a substring.
-  await expect(page.getByRole('link', { name: 'Blicky' })).toBeVisible
+  await expect(page.getByRole('link', { name: 'Blicky' })).toBeVisible()
+  console.log(await page.getByRole('link', { name: 'Blicky' }).textContent())
 })
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/')
+test('game modes are visible', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click()
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole('heading', { name: 'Installation' })
-//   ).toBeVisible()
-// })
+  await page.getByRole('link', { name: 'Choose Game Mode' }).click()
+  await expect(
+    page.getByRole('heading', { level: 2, name: 'Game Modes' })
+  ).toBeVisible()
+  console.log('test is running')
+  await expect(page.getByRole('link', { name: 'Clicky' })).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Bounce' })).toBeVisible()
+})
