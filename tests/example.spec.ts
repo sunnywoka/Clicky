29409c70db96c9b317353308aa5847b9b06dee59
Humaid Memee <humaidmemee@gmail.com>
2023-09-18 15:54:46 +1200

    add test for initial click score

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,22 @@
 import { test, expect } from '@playwright/test'
 
+import sinon from 'sinon'
+import * as Path from 'node:path/posix'
+import * as URL from 'node:url'
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
+test.beforeEach(async ({ context }) => {
+  // Install Sinon in all the pages in the context
+  await context.addInitScript({
+    path: Path.join(__dirname, '..', './node_modules/sinon/pkg/sinon.js'),
+  })
+  // Auto-enable sinon right away
+  await context.addInitScript(() => {
+    window.__clock = sinon.useFakeTimers()
+  })
+})
 test('home has title', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
@@ -36,14 +53,13 @@ test('clicky game displays all starting elements', async ({ page }) => {
   ).toBeVisible()
 })
 
-test.only('clicky game displays correct score on initial click', async ({
+test('clicky game displays correct score on initial click', async ({
   page,
 }) => {
   await page.goto('http://localhost:5173/clicky')
 
   await page.getByRole('button', { name: 'Start' }).click()
   await page.getByTestId('square-rect').click()
-
   await expect(
     page.getByRole('heading', { level: 2, name: 'Score: 100' })
   ).toBeVisible()
