050951aa06352b3146dab4f02b0f690729a35c3c
Humaid Memee <humaidmemee@gmail.com>
2023-09-18 14:13:55 +1200

    add test for starting clicky game

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,6 @@ test('home has title', async ({ page }) => {
   await page.goto('http://localhost:5173/')
 
   await expect(page.getByRole('link', { name: 'Blicky' })).toBeVisible()
-  console.log(await page.getByRole('link', { name: 'Blicky' }).textContent())
 })
 
 test('game modes are visible', async ({ page }) => {
@@ -14,7 +13,19 @@ test('game modes are visible', async ({ page }) => {
   await expect(
     page.getByRole('heading', { level: 2, name: 'Game Modes' })
   ).toBeVisible()
-  console.log('test is running')
   await expect(page.getByRole('link', { name: 'Clicky' })).toBeVisible()
   await expect(page.getByRole('link', { name: 'Bounce' })).toBeVisible()
+})
+
+test('clicky game displays all starting elements', async ({ page }) => {
+  await page.goto('http://localhost:5173/category')
+
+  await page.getByRole('link', { name: 'Clicky' }).click()
+  await expect(
+    page.getByRole('heading', { level: 1, name: 'Clicky!' })
+  ).toBeVisible()
+  await page.getByRole('button', { name: 'Start' }).click()
+  await expect(
+    page.getByRole('heading', { level: 1, name: 'Clicky!' })
+  ).toBeVisible()
 })
