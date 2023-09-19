c19ea1a0b8d27499d8860e7c5c80ba59f5a2fa4a
Yuekai Wu <yuekaixx@outlook.com>
2023-09-20 11:20:28 +1200

    add selecting feature to table

--- client/components/ScoreRanking.tsx
+++ client/components/ScoreRanking.tsx
@@ -3,6 +3,7 @@ import useRecord from './hooks/useRecords'
 import { Record } from '../../models/Record'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 function ScoreRanking() {
   function compareFn(a: Record, b: Record) {
@@ -10,6 +11,27 @@ function ScoreRanking() {
   }
 
   const records = useRecord()
+
+  const [selectedNickname, setSelectedNickname] = useState('')
+  const nicknames = [
+    ...new Set(records.data?.map((score: Record) => score.nickname || '')),
+  ] as string[]
+
+  const handleNicknameChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedNickname(event.target.value)
+  }
+
+  const [selectedGameId, setSelectedGameId] = useState('')
+  const gameIds = [
+    ...new Set(
+      records.data?.map((score: Record) => score.gameId.toString() || '')
+    ),
+  ] as string[]
+  const handleGameIdChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGameId(event.target.value)
+  }
 
   return (
     <>
@@ -23,42 +45,76 @@ function ScoreRanking() {
           Score Ranking
         </h2>
         <table className="mx-auto">
-          <tr>
-            <th className="border-l-4 border-y-4 text-4xl font-bold text-primary border-primary px-12 py-6">
-              Order
-            </th>
-            <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
-              Nickname
-            </th>
-            <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
-              Score
-            </th>
-            <th className="border-y-4 border-r-4 text-4xl font-bold text-primary border-primary px-24 py-12">
-              Game mode
-            </th>
-          </tr>
-          {records.data
-            ?.sort(compareFn)
-            ?.map((Score: Record, index: number, array: Record[]) => (
-              <tr key={Score.nickname}>
-                <td className="border-y-2 border-l-2 text-4xl text-primary border-primary px-24 py-4">
-                  {index + 1}
-                </td>
-                <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
-                  {Score.nickname}
-                </td>
-                <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
-                  {Score.score}
-                </td>
-                <td className="border-y-2 border-r-2 text-4xl text-primary border-primary px-24 py-4">
-                  {Score.gameId === 1
-                    ? 'Clicky'
-                    : Score.gameId === 2
-                    ? 'Bouncy'
-                    : 'Shrinky'}
-                </td>
-              </tr>
-            ))}
+          <thead>
+            <tr>
+              <th className="border-l-4 border-y-4 text-4xl font-bold text-primary border-primary px-12 py-6">
+                Order
+              </th>
+              <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
+                <select
+                  value={selectedNickname}
+                  onChange={handleNicknameChange}
+                >
+                  <option value={''}>Nickname</option>
+                  {nicknames.map((nickname, index) => (
+                    <option value={nickname} key={index}>
+                      {nickname}
+                    </option>
+                  ))}
+                </select>
+              </th>
+              <th className="border-y-4 text-4xl font-bold text-primary border-primary px-24 py-12">
+                Score
+              </th>
+              <th className="border-y-4 border-r-4 text-4xl font-bold text-primary border-primary px-24 py-12">
+                <select value={selectedGameId} onChange={handleGameIdChange}>
+                  <option value={''}>Game Mode</option>
+                  {gameIds.map((id, index) => (
+                    <option value={id} key={index}>
+                      {id == '1' ? 'Clicky' : id == '2' ? 'Bouncy' : 'Shrinky'}
+                    </option>
+                  ))}
+                </select>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.data &&
+              records.data
+                .sort(compareFn)
+                .filter((Score: Record) => {
+                  if (selectedNickname && Score.nickname !== selectedNickname) {
+                    return false
+                  }
+                  if (
+                    selectedGameId &&
+                    Score.gameId.toString() !== selectedGameId
+                  ) {
+                    return false
+                  }
+                  return true
+                })
+                .map((Score: Record, index: number) => (
+                  <tr key={index}>
+                    <td className="border-y-2 border-l-2 text-4xl text-primary border-primary px-24 py-4">
+                      {index + 1}
+                    </td>
+                    <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
+                      {Score.nickname}
+                    </td>
+                    <td className="border-y-2 text-4xl text-primary border-primary px-24 py-4">
+                      {Score.score}
+                    </td>
+                    <td className="border-y-2 border-r-2 text-4xl text-primary border-primary px-24 py-4">
+                      {Score.gameId === 1
+                        ? 'Clicky'
+                        : Score.gameId === 2
+                        ? 'Bouncy'
+                        : 'Shrinky'}
+                    </td>
+                  </tr>
+                ))}
+          </tbody>
         </table>
         <Link to="/" className="home-btn mx-auto">
           Back
