b2ea0ff7b37f8242761cda1d03dd40f2d551ca7a
Humaid Memee <humaidmemee@gmail.com>
2023-09-20 13:45:19 +1200

    fix tests

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, mergeConfig } from 'vite'
+import viteConfig from './vite.config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3000',
+      },
     },
-  },
-})
+    test: {
+      exclude: [
+        '**/tests/**/*',
+        '**/tests-examples/**/*',
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      ],
+    },
+  })
+)
